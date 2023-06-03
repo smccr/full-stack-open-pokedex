ae64fa9794a15a3667a2da4e7df75538a22f477d
Sergio <smccr123@gmail.com>
2023-06-03 13:32:51 -0600

    Exercise 11.9

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+/* eslint-disable no-undef */
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
